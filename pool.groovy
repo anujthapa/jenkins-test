422cfbdae7c04be10e1e0c1dbf9047c8f5428642
anujthapa <33247998+anujthapa@users.noreply.github.com>
2021-11-29 13:05:12 +0200

    Update pool.groovy

--- pool.groovy
+++ pool.groovy
@@ -1,6 +1,11 @@
-import groovy.json.JsonSlurperClassic
-httpRequest url: 'http://localhost:8080/job/test-job/api/json', outputFile: 'output.json'
+pipeline {
+    agent any
 
-def jsonFile = readFile(file: 'output.json')
-def data = new JsonSlurperClassic().parseText(jsonFile)
-latestBuildNumber = "${data.lastSuccessfulBuild.number}"
+    stages {
+        stage('Hello') {
+            steps {
+                echo 'Hello World'
+            }
+        }
+    }
+}
