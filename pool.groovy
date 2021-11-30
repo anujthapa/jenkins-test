c1620176b738c280a69cb2cebf38c2421c3bd843
anujthapa <33247998+anujthapa@users.noreply.github.com>
2021-11-30 14:44:32 +0200

    Update pool.groovy

--- pool.groovy
+++ pool.groovy
@@ -4,7 +4,7 @@ pipeline {
         stage('Hello') {
             steps {
                 echo 'Hello World'
-                println("dry_Run"+env.dryRun)
+                println("dry_Run"+env.name)
             }
         }
     }
